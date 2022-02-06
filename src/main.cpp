0b11c2e2ffc66bc6885d0794c5b99ad1f9463dff
Khalyn <piau.melvyn@gmail.com>
2022-02-05 19:15:35 -0500

    Testing the interface by concepts
    Replacing the interface by a concept definition. It's working !

--- src/main.cpp
+++ src/main.cpp
@@ -19,19 +19,54 @@ void display_msgbox_example()
 }
 
 
-template<CTemperature T>
-constexpr void display_current_temperature( T const &temperature )
+constexpr void display_current_temperature( CTemperature auto const &temperature )
 {
-	printf( "Temperature: %.2f%s\n", temperature.as_inner(), T::UNIT );
+	printf( "Temperature: %.2f%s\n", temperature.as_inner(), temperature.UNIT );
 }
+
+struct A { float add() const { return 0.f; } };
+struct B
+{
+	static constexpr u32 BYTES = u32( 1u );
+	static constexpr u32 BITS = u32( BYTES * 8 );
+
+	constexpr void SerializeBool( char *, boolean &bb ) { }
+	constexpr void SerializeU32( char *, u32 &bb ) { }
+
+	bool a = true;
+};
+
+using namespace std;
+template<typename T> concept CInterface =
+requires ( T &test )
+{
+	// Variables
+	requires is_same_v<decltype( T::BYTES ), u32 const>;
+	requires is_same_v<decltype( T::BITS ), u32 const>;
+
+	// Methods
+	{ test.SerializeBool( declval<char *>(), declval<boolean&>() ) } -> same_as<void>;
+	{ test.SerializeU32( declval<char*>(), declval<u32&>() ) } -> same_as<void>;
+};
+
+static_assert(CInterface<B>);
+
+constexpr void xxx( CInterface auto const &test )
+{
+}
+
 
 int main()
 {
 	constexpr Celsius celsius( 15.0f );
 	constexpr Fahrenheit fahrenheit = celsius_to_fahrenheit( celsius );
-	display_current_temperature( fahrenheit );
+	display_current_temperature<Fahrenheit>( fahrenheit );
 	display_current_temperature( celsius );
 
 	display_msgbox_example();
+	A a;
+	B b;
+	xxx( b );
+
     return 0;
 }
